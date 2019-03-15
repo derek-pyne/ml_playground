13af446cb1d21485f1d4624b37e0f1bf1154db4a
Derek Pyne <derek.pyne@shopify.com>
2019-03-15 11:15:14 -0400

    Added deep learning course

---
+++ classes/deep_learning/lectures/03 - Convolutional Neural Networks/plotting.py
@@ -0,0 +1,26 @@
+# Common imports
+import numpy as np
+import os
+
+# To plot pretty figures
+import matplotlib
+import matplotlib.pyplot as plt
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+
+
+def save_fig(fig_id, tight_layout=True):
+    path = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID, fig_id + ".png")
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format='png', dpi=300)
+    
+def plot_image(image):
+    plt.imshow(image, cmap="gray", interpolation="nearest")
+    plt.axis("off")
+
+def plot_color_image(image):
+    plt.imshow(image.astype(np.uint8),interpolation="nearest")
+    plt.axis("off")
---
+++ classes/deep_learning/lectures/03 - Convolutional Neural Networks/setups.py
@@ -0,0 +1,20 @@
+# Common imports
+import numpy as np
+import os
+
+# to make this notebook's output stable across runs
+def reset_graph(seed=42):
+    tf.reset_default_graph()
+    tf.set_random_seed(seed)
+    np.random.seed(seed)
+
+# To plot pretty figures
+import matplotlib
+import matplotlib.pyplot as plt
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+
+#Import tensorflow
+import tensorflow as tf
+    
